9728abfed475226de976f5da6d40a08a10b9f0c6
Raiga <raiga0310@icloud.com>
2019-04-28 14:16:11 +0900

    resolve

--- B.c
+++ B.c
@@ -6,13 +6,14 @@ int main(){
     for(int i=0;i<n;i++){
     	scanf("%d",&juwel[i]);
     }
-    for(i=0;i<n;i++){
-    	scanf("%d",&jcost[i]);
+    for(int j=0;j<n;j++){
+    	scanf("%d",&cost[j]);
     }
-    for(i=0;i<n;i++){
-    	if(juwel[i]-cost[i]>=0){
-    		count++;
+    for(int l=0;l<n;l++){
+    	if(juwel[l]-cost[l]>=0){
+    		count+=(juwel[l]-cost[l]);
     	}
     }
+    printf("%d",count);
     return 0;
 }
